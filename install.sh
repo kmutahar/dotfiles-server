4272cee25ba2b22328805848c01c2f25a4aa01fe
Khaldoon Mutahar <git@mutahar.me>
2020-03-03 18:06:01 +0300

    update install command

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Update Server
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+# Setup zsh with ohmyzsh
+sudo apt install zsh
+sudo chsh -s /bin/zsh ubuntu 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install The Fuck
 sudo apt install python3-dev python3-pip python3-setuptools -y
 sudo pip3 install thefuck
@@ -8,10 +18,8 @@ sudo pip3 install thefuck
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# Install MEGA
+wget  https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megacmd-xUbuntu_18.04_amd64.deb && sudo apt install ./megacmd-xUbuntu_18.04_amd64.deb 
+
 # symlink files
-
-# bash profile
-ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
-
-# bash profile (root user)
-sudo ln -sf /home/ubuntu/.dotfiles/.bash_profile /root/.bash_profile
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
