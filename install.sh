f5ca42e9c13168ea0a811abda653e18031b256bd
Khaldoon Mutahar <git@mutahar.me>
2020-03-04 20:18:40 +0300

    cleanup mega install

--- install.sh
+++ install.sh
@@ -22,7 +22,9 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # Install MEGA
-wget https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megacmd-xUbuntu_18.04_amd64.deb && sudo apt install ./megacmd-xUbuntu_18.04_amd64.deb
+wget https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megacmd-xUbuntu_18.04_amd64.deb
+sudo apt install ./megacmd-xUbuntu_18.04_amd64.deb -y
+rm megacmd-xUbuntu_18.04_amd64.deb
 
 # Install youtube-dl
 sudo apt-get install python-pip
