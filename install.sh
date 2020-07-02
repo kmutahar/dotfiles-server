b3f157bf5e16731f8cb5ce5268f7248e13ab6c76
Khaldoon Mutahar <git@mutahar.me>
2020-07-02 16:45:23 +0300

    Add Lazygit to install.sh

--- install.sh
+++ install.sh
@@ -24,5 +24,9 @@ sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo chsh -s /bin/zsh ubuntu 
 
+# Install LazyGit
+sudo add-apt-repository ppa:lazygit-team/release
+sudo apt-get install lazygit
+
 # symlink files
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
