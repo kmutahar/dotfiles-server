ddd6f56a3010413056b3c7c2cfbeef552a3e7dd9
Khaldoon Mutahar <git@mutahar.me>
2020-07-03 17:29:27 +0300

    change install order to make zsh and ohmyzsh first thing

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
+# Setup zsh with ohmyzsh
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo chsh -s /bin/zsh ubuntu
+
 # Install The Fuck
 sudo apt install python3-dev python3-pip python3-setuptools -y
 sudo pip3 install thefuck
@@ -18,11 +23,6 @@ sudo pip3 install thefuck
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-
-# Setup zsh with ohmyzsh
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-sudo chsh -s /bin/zsh ubuntu 
 
 # Install LazyGit
 sudo add-apt-repository ppa:lazygit-team/release
