2c04a03fed5168c413e8825c15c9c405bfe01b0d
Khaldoon Mutahar <git@mutahar.me>
2020-09-03 12:27:34 +0300

    👌 IMPROVE: Removed extra white space from cron job output

--- cron/daily-update.sh
+++ cron/daily-update.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Update OS
-printf "`/bin/date +\%F` @ `/bin/date +\%H`:`/bin/date +\%M` UTC+3 \n"
+printf "`/bin/date +\%F` @ `/bin/date +\%H`:`/bin/date +\%M` UTC+3\n"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 printf "\n"
 
 # Update NPM
-printf "`/bin/date +\%F` @ `/bin/date +\%H`:`/bin/date +\%M` UTC+3 \n"
+printf "`/bin/date +\%F` @ `/bin/date +\%H`:`/bin/date +\%M` UTC+3\n"
 sudo npm install -g npm
 printf "\n"
 
 # Update Ghost CMS
-printf "`/bin/date +\%F` @ `/bin/date +\%H`:`/bin/date +\%M` UTC+3 \n"
+printf "`/bin/date +\%F` @ `/bin/date +\%H`:`/bin/date +\%M` UTC+3\n"
 cd /var/www/ghost/
 sudo npm install -g ghost-cli@latest
 ghost update
